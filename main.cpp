6d7d2dc94fe8728361829d40a91bb69624d11af9
Suvorova-Sofya <suvorova.sofya@yandex.ru>
2019-09-27 19:39:28 +0300

    Изменения, которые будут включены в коммит: 	изменено:      main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ complex::complex(double r1, double q1){
     q=q1;
 }
 
-void complex::print_complex() {
+complex complex::print_complex() {
     std::cout << r << " " << q << std::endl;
 }
 
-void complex::sum_complex(complex a) {
+complex complex::sum_complex(complex a) {
     complex res;
     double a1,b1,a2,b2,a3,b3;
     a1=r*cos(q*M_PI);
@@ -24,10 +24,10 @@ void complex::sum_complex(complex a) {
     b3=b1+b2;
     res.r=sqrt(pow(a3,2)+pow(b3,2));
     res.q=atan(b3/a3);
-    std::cout << res.r << " " << res.q << std::endl;
+    return res;
 }
 
-void complex::diff_complex(complex a) {
+complex complex::diff_complex(complex a) {
     complex res;
     double a1,b1,a2,b2,a3,b3;
     a1=r*cos(q*M_PI);
@@ -38,57 +38,69 @@ void complex::diff_complex(complex a) {
     b3=b1-b2;
     res.r=sqrt(pow(a3,2)+pow(b3,2));
     res.q=atan(b3/a3);
-    std::cout << res.r << " " << res.q << std::endl;
+    return res;
 }
 
-void complex::mult_complex(complex a) {
+complex complex::mult_complex(complex a) {
     complex res;
     res.r=a.r*r;
     res.q=a.q+q;
-    std::cout << res.r << " " << res.q << std::endl;
+    return res;
 }
 
-void complex::divis_complex(complex a) {
+complex complex::divis_complex(complex a) {
     complex res;
     res.r=r/a.r;
     res.q=q-a.q;
-    std::cout << res.r << " " << res.q << std::endl;
+    return res;
 }
 
-void complex::compar_complex(complex a) {
+bool complex::compar_complex(complex a) {
     if((r==a.r)&&(q==a.q)){
-        std::cout << "Равны" << std::endl;
+        return true;
     } else{
-        std::cout << "Не равны" << std::endl;
+        return false;
     }
 }
 
-void complex::conj_complex() {
-    q=(-1)*q;
+complex complex::conj_complex() {
+    complex res;
+    res.r=r;
+    res.q=(-1)*q;
+    return res;
 }
 
 int main() {
     double a, b,c,d;
-    std::cout << "Введите 2 комплексных числа"<< std::endl;
+    std::cout << "Введите 2 комплексных числа" << std::endl;
     std::cin >> a >> b >> c >> d;
     if((a<0) || (c<0)){
-        std::cout << "Модуль не может быть отрицательным" << std::endl;
+        std::cout << "Модуль не может быть отрицательным " << std::endl;
     }else {
         complex w1 = complex(a, b);
         complex w2 = complex(c, d);
+        complex res;
         std::cout << "+ :";
-        w1.sum_complex(w2);
+        res=w1.sum_complex(w2);
+        res.print_complex();
         std::cout << "- :";
-        w1.diff_complex(w2);
+        res=w1.diff_complex(w2);
+        res.print_complex();
         std::cout << "* :";
-        w1.mult_complex(w2);
+        res=w1.mult_complex(w2);
+        res.print_complex();
         std::cout << "/ :";
-        w1.divis_complex(w2);
+        res=w1.divis_complex(w2);
+        res.print_complex();
         std::cout << "Сравнение :";
-        w1.compar_complex(w2);
+        if(w1.compar_complex(w2)){
+            std::cout << "Равны" <<"\n";
+        }else{
+            std::cout <<"Не равны" << "\n";
+        }
         std::cout << "Сопряженое первого комплексного числа :";
-        w1.conj_complex();
-        w1.print_complex();
+        res=w1.conj_complex();
+        res.print_complex();
     }
     return 0;
 }
