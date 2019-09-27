21a4c3f4a165b3d8459201b108257aa9192e83c1
Suvorova-Sofya <suvorova.sofya@yandex.ru>
2019-09-27 19:38:10 +0300

    Изменения, которые будут включены в коммит: 	изменено:      complex.h

--- complex.h
+++ complex.h
@@ -5,13 +5,13 @@ struct complex{
 
     complex();
     complex(double r1,double q1);
-    void print_complex();
-    void sum_complex(complex a);
-    void diff_complex(complex a);
-    void mult_complex(complex a);
-    void divis_complex(complex a);
-    void compar_complex(complex a);
-    void conj_complex();
+    complex print_complex();
+    complex sum_complex(complex a);
+    complex diff_complex(complex a);
+    complex mult_complex(complex a);
+    complex divis_complex(complex a);
+    bool compar_complex(complex a);
+    complex conj_complex();
 private:
     double r;
     double q;
